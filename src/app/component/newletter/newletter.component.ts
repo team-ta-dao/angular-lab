0314769516c69dc0c009437d980118ba9c7f054f
tunv <nguyenvantu191298@gmail.com>
2021-03-19 12:56:39 +0700

    homepages

---
+++ src/app/component/newletter/newletter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newletter',
+  templateUrl: './newletter.component.html',
+  styleUrls: ['./newletter.component.scss']
+})
+export class NewletterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
